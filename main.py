2723c9556c210bf784d32154e08c2e6b21c95451
sudipkarki123 <sudipkarkicoding@gmail.com>
2024-12-07 11:55:06 +0545

    lesson 3

---
+++ main.py
@@ -0,0 +1,28 @@
+# a=5
+# print("Type of A=",type(a))
+# b='5'
+# print("Type of b=",type(b))
+# c=False
+# print("Type of c=",type(c))
+# d=6.7
+# print("Type of d=",type(d))
+
+
+# a=5
+# print(float(a))#int to float
+# amt=876.50
+# print(int(amt))#flot to int
+# value='78'
+# print(int(value))#str to int
+# n=5
+# print(str(n))#int to str
+
+# word=input("Enter the word:")
+# print("Reversed=",word[word::-1])
+
+# text="congratulations"
+# print(text[4:11:2])
+
+# word="football"
+# print(word[4:8:1])
+# print(word[4::1])
